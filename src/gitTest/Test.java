6ec912a9e5e5f8b7250a325fe527e9db06b03e6a
Philipp <philipp@DESKTOP-INDPBNP>
2023-10-07 14:17:43 +0200

    windoof

--- src/gitTest/Test.java
+++ src/gitTest/Test.java
@@ -3,12 +3,8 @@ package gitTest;
 public class Test {
 
 	public static void main(String[] args) {
-//Änderung1
-
 		// ÄnderungWindows
-		//ÄnderungWindow2
-		// ÄnderungLenovo
-		// ÄnderungLenovo2
+
 	}
 
 }
