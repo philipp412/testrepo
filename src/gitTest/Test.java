e9eb94bac11ad6404b8ee0a9666caeaf4518460f
philipp <philipp@philipp-Lenovo>
2023-10-07 14:18:24 +0200

    c

--- src/gitTest/Test.java
+++ src/gitTest/Test.java
@@ -3,12 +3,9 @@ package gitTest;
 public class Test {
 
 	public static void main(String[] args) {
-//Änderung1
 
-		// ÄnderungWindows
-		//ÄnderungWindow2
 		// ÄnderungLenovo
-		// ÄnderungLenovo2
+
 	}
 
 }
