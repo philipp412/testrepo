1e8814f3edb5557eb6d078fec516b21983f1a57e
Philipp <philipp@DESKTOP-INDPBNP>
2023-10-09 17:47:50 +0200

    jg

--- src/gitTest/Test.java
+++ src/gitTest/Test.java
@@ -3,11 +3,9 @@ package gitTest;
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		// ÄnderungWindows
-=======
 		// ÄnderungLenovo1
->>>>>>> branch 'master' of https://github.com/philipp412/testrepo.git
 
 	}
 
